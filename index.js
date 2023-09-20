2b78e72a03f5b37955511afe49c47d905e186d10
Sugey Martinez Briones <isc20350293@gmail.com>
2023-09-20 13:05:40 -0600

    Query params

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-//http://localhost:3000/julio
+//http://localhost:3000/sugey
 app.get('/', (req, res)=> {
     res.json({msg: 'Hello GET'})
 })
@@ -15,6 +15,19 @@ app.get('/', (req, res)=> {
     res.json({msg: 'Fallo en el usuario o contraseña'})
 })
 
+//http://localhost:3000/login?user=Sugey&password=123
+app.get('/login', (req, res) => {
+    const {user, password} = req.query
+    if (!user || !password){
+        res.json({msg: 'You need to provide <user> and <password> params'})
+    }
+    
+if(user === 'Sugey' && password === '123'){
+    res.json({msg: 'Sing In succesfull'})
+    return
+}
+res.status(404).json({msg: 'wrong user or password'})
+})
 app.post('/',(req, res)=>{
     res.json({msg:'Hello POST'})
 })
